5a4f022a53154a6a47615f9b6c965a7b488c1aab
web3santa <usefulstory35@gmail.com>
2024-04-05 04:38:40 +0900

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,67 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-export default function Home() {
+
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+
+  await new Promise((resolve) => setTimeout(resolve,3000))
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
   return (
-    <main>Home</main>
-  )
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="recipe img"/>
+                <AvatarFallback>
+                  {recipe.title.slice(0,2)}
+                </AvatarFallback>
+              </Avatar>
+
+              <div>
+              <CardTitle>{recipe.title}</CardTitle>
+              <CardDescription>
+                {recipe.time} mins to cook.
+              </CardDescription>
+
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button  >Veiw Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
